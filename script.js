d498d866d45245ba99628f229ac89d018fce4afd
John Paul O. Milan <89463014+JpMiLAN@users.noreply.github.com>
2022-08-31 01:39:18 +0800

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["DiskJockey/Operator", "Developer", "Blogger", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
