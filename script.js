b9eea8433e18901c73ba46b1cb0e6ea4ae50816e
John Paul O. Milan <89463014+JpMiLAN@users.noreply.github.com>
2023-04-15 05:30:51 -0700

    Update script.js

--- script.js
+++ script.js
@@ -33,20 +33,7 @@ $(document).ready(function(){
         $('.menu-btn i').toggleClass("active");
     });
 
-    // typing text animation script
-    var typed = new Typed(".typing", {
-        strings: [ "Data Analytics", "Developer", "Designer", "Freelancer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
-
-    var typed = new Typed(".typing-2", {
-        strings: ["App Dev", "Back End", "Front End", "Web Dev"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
+    
 
     // owl carousel script
     $('.carousel').owlCarousel({
